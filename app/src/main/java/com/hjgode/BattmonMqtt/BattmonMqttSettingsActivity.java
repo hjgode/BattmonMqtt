509de0b0dcf58a4cba21e654eb63ded8d54ea611
hgode <hjgode@gmail.com>
2020-08-24 19:17:21 +0200

    add preOnChangeListener for mqtt_port

--- app/src/main/java/com/hjgode/BattmonMqtt/BattmonMqttSettingsActivity.java
+++ app/src/main/java/com/hjgode/BattmonMqtt/BattmonMqttSettingsActivity.java
@@ -2,14 +2,17 @@ package com.hjgode.BattmonMqtt;
 
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.preference.Preference;
+//import android.preference.EditTextPreference;
+import androidx.preference.EditTextPreference;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NavUtils;
 import androidx.preference.CheckBoxPreference;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
 public class BattmonMqttSettingsActivity extends AppCompatActivity {
@@ -38,43 +41,35 @@ public class BattmonMqttSettingsActivity extends AppCompatActivity {
     }
 
 
-    public static class SettingsFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
+    public static class SettingsFragment extends PreferenceFragmentCompat  {
+
+        @Override
+        public void onCreate(Bundle SavedInstance){
+            super.onCreate(SavedInstance);
+        }
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
-        }
 
-        @Override
-        public boolean onPreferenceChange(Preference preference, Object o) {
-            boolean validated=true;
-            if (preference != null) {
-                String key = findPreference(preference.getKey()).toString();
-                String value=o.toString();
-                util.LOG( "SettingsFragment onSharedPreferenceChanged called with key="+key+", new value="+value);
-                if(key.equals(getResources().getString(R.string.mqtt_port))){
-                    int test=0;
+            final SharedPreferences prefs=getContext().getSharedPreferences("gameSettings",0);
+            final SharedPreferences.Editor prefsedit=prefs.edit();
+            EditTextPreference port= (EditTextPreference) findPreference("mqtt_port");
+            port.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+                @Override
+                public boolean onPreferenceChange(Preference preference, Object newValue) {
                     try{
-                        Integer.parseInt(value);
+                        int p = Integer.parseInt(newValue.toString());
+                        prefsedit.putInt(getResources().getString(R.string.mqtt_port), p);
+                        prefsedit.apply();
+                        Toast.makeText(getContext(),newValue+" set as new port",Toast.LENGTH_LONG);
+                        return true;
                     }catch(Exception ex){
-                        validated=false;
-                    }
-                    if(test<1 || test>65536){
-                        validated=false;
+                        Toast.makeText(getContext(),newValue+" is not a vallid port",Toast.LENGTH_LONG);
+                        return false;
                     }
                 }
-                else if(key.equals(getResources().getString(R.string.mqtt_interval))){
-                    int test=0;
-                    try{
-                        Integer.parseInt(value);
-                    }catch(Exception ex){
-                        validated=false;
-                    }
-                    if(test<1 || test>65536){
-                        validated=false;
-                    }
-                }
-            }
-            return validated; //true=update and commit
+            });
+
         }
     }
 }
